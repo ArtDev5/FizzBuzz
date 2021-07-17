f78d2002a503541d675b561e2a94bc88aeecc9f8
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-07-17 13:03:46 -0300

    Class refactor for add new requirements

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,9 +3,9 @@ public class FizzBuzz {
     public String getFizzBuzz(int number){
         if(number % 15 == 0){
             return "FizzBuzz";
-        }else if(number % 3 == 0){
+        }else if(number % 3 == 0 || String.valueOf(number).contains("3")){
             return "Fizz";
-        }else if(number % 5 == 0){
+        }else if(number % 5 == 0 || String.valueOf(number).contains("5")){
             return "Buzz";
         }
         return String.valueOf(number);
