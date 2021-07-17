e1c2f2b73c03f46e79b8abad3d3d345b08bbfc04
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-07-17 12:22:14 -0300

    Fizz - GREEN

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,9 @@
 public class FizzBuzz {
 
     public String getFizzBuzz(int number){
-
+        if(number == 3){
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
