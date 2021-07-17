e963969a0a39cc66fa1e00bc81b3f3a0531c84d7
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-07-17 12:25:58 -0300

    Buzz - GREEN

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,6 +3,8 @@ public class FizzBuzz {
     public String getFizzBuzz(int number){
         if(number % 3 == 0){
             return "Fizz";
+        }else if(number == 5){
+            return "Buzz";
         }
         return String.valueOf(number);
     }
