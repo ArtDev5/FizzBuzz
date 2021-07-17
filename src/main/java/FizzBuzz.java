d8397ef4d9ffd167e54ccc8ec0b624be6e74ad59
ArtDev5 <artur_antoniopw01@hotmail.com>
2021-07-17 12:33:15 -0300

    FizzBuzz - REFACTOR

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
 
     public String getFizzBuzz(int number){
-        if(number == 15){
+        if(number % 15 == 0){
             return "FizzBuzz";
         }else if(number % 3 == 0){
             return "Fizz";
